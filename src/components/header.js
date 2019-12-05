9977dd5c037531bac3f2785fe9a8d9a9328549bd
TKFM21 <tkzw21@gmail.com>
2019-12-05 21:23:18 +0900

    tutorial complete

---
+++ src/components/header.js
@@ -0,0 +1,2 @@
+import React from 'react';
+export default props => <h1>{props.headerText}/{props.arbitraryPhrase}</h1>;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
+import Header from '../components/header';
+import Layout from '../components/layout';
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <Layout>
+        <h1>
+            Hello world! Gatsby!
+        </h1>
+        <Header headerText="Propsで取得" />
+        <p>What a world...</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <br/>
+        <Link to="/about-css-modules">About</Link>
+        <br/>
+        <Link to="/contact">Contact</Link>
+    </Layout>
+);
